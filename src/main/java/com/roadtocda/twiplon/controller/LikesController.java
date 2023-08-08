a22e1665b15fb8e078e9543b5a52cea93666597b
gaouimounir <mounir.gaoui@outlook.com>
2023-08-08 16:21:23 +0200

    feat: add likes controller function mapping

---
+++ src/main/java/com/roadtocda/twiplon/controller/LikesController.java
@@ -0,0 +1,26 @@
+package com.roadtocda.twiplon.controller;
+	
+	import java.util.Map;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RestController;
+
+	import com.roadtocda.twiplon.service.LikesService;
+
+	@RestController
+	public class LikesController {
+
+		@Autowired
+		private LikesService likeService;
+		
+		@PostMapping("/like")
+		public void Likes(@RequestBody Map<String, Long> requestBody) {
+			Long postId = requestBody.get("postId");
+			likeService.likeSave(4, postId.intValue());
+		}
+		
+	}
+
+
