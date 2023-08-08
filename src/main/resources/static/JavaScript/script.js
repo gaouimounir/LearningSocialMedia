3dd61c4bcee5bbd17a4f7c1c7d2cc9fcdfffbc87
gaouimounir <mounir.gaoui@outlook.com>
2023-08-08 16:14:36 +0200

    feat: add count likes

--- src/main/resources/static/JavaScript/script.js
+++ src/main/resources/static/JavaScript/script.js
@@ -1,3 +1,29 @@
+document.querySelectorAll(".btnLike").forEach((btnLike) => {
+  btnLike.addEventListener("click", function () {
+    console.log(btnLike.dataset.idpost);
+    fetch("/like", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ postId: btnLike.dataset.idpost }),
+    })
+      .then((response) => response.json())
+      .then((data) => {});
+  });
+});
+
+document.querySelectorAll(".fa-heart").forEach((heartChange) => {
+  heartChange.addEventListener("click", function () {
+    heartChange.classList.remove("fa-regular");
+    heartChange.classList.add("fa-solid");
+  });
+});
+
+
+
+/*------------------*/
+
 let myComment = document.querySelector("textarea");
 let myCommentButton = document.querySelector(".btnComment");
 
