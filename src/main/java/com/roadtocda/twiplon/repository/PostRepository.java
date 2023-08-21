8c2d33c31212725a8285ae678a7972f15e698dea
gaouimounir <mounir.gaoui@outlook.com>
2023-08-21 21:27:30 +0200

    feat: update post repository

--- src/main/java/com/roadtocda/twiplon/repository/PostRepository.java
+++ src/main/java/com/roadtocda/twiplon/repository/PostRepository.java
@@ -1,12 +1,16 @@
 package com.roadtocda.twiplon.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.roadtocda.twiplon.model.Post;
+import com.roadtocda.twiplon.model.Users;
 
 @Repository
 public interface PostRepository extends CrudRepository <Post, Long> {
-	
+	Iterable<Post> findAllByOrderByDatecreationDesc();
+	List<Post> findByUserOrderByDatecreationDesc(Users user);	
 
 }
